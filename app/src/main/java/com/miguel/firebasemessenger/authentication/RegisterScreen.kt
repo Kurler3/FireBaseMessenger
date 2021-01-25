0c932402288f003527460aacbcbff3bc67c3e639
Miguel Bento <miguelgb2000@gmail.com>
2021-01-25 21:16:02 +0800

    Initial commit

---
+++ app/src/main/java/com/miguel/firebasemessenger/authentication/RegisterScreen.kt
@@ -0,0 +1,16 @@
+package com.miguel.firebasemessenger.authentication
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.miguel.firebasemessenger.R
+
+class RegisterScreen : Fragment(R.layout.register_screen) {
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
